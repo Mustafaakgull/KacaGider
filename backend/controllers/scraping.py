e5bdf82f55a48ffec8c14221b5bcbe2f0cecfb74
Ömer Faruk Yılmaz <omerfarukyy@hotmail.com>
2025-05-25 15:00:16 +0300

    make scraping fully functional

--- backend/controllers/scraping.py
+++ backend/controllers/scraping.py
@@ -1,40 +1,50 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
+import random
+from backend.models.redis_client import redis_client
 
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach", True)
-# driver = webdriver.Chrome(options=chrome_options)
 
-URL = "https://www.arabam.com/ilan/galeriden-satilik-fiat-punto-grande-1-4-fire-dynamic/2006-fiat-punto-grande-1-4-benzin-lpg-fire-dynamic/29752672"
+def scrape_vehicle(type):
 
-response = requests.get(URL)
-res = response.text
+    if type == "":
+        type = "otomobil"
 
-soup = BeautifulSoup(res, "html.parser")
-# print(soup)
-photos = soup.find_all("div", class_="swiper-slide")
-values = soup.find_all("div", class_="property-item")
+    page_num = random.randint(1, 50)
+    list_item_num = random.randint(0, 49)
+    info_key_list = []
+    info_value_list = []
+    photo_link_list = []
+    car_page_links = []
 
-info_key_list = []
-info_value_list = []
-for info in values:
-    info_key = info.find_all('div', class_="property-key")
-    info_key = [info.get_text(strip=True) for info in info_key]
-    info_key_list.append(info_key[0])
-    info_value = info.find_all('div', class_="property-value")
-    info_value = [info.get_text(strip=True) for info in info_value]
-    info_value_list.append(info_value[0])
-result = dict(zip(info_key_list, info_value_list))
-print(result)
+    main_url_for_cars = f"https://www.arabam.com/ikinci-el/{type}?take=50&page={page_num}"
+    main_response = requests.get(main_url_for_cars)
+    main_res = main_response.text
+    soup1 = BeautifulSoup(main_res, 'html.parser')
+    listed_cars = soup1.find_all('tr', class_="listing-list-item")
 
-photo_link_list = []
-for photo in photos:
-    photo_link_list.append(photo.find_all("img")[0].get("data-src"))
+    for cars in listed_cars:
+        car_page_links.append(cars.find('a')['href'])
 
-photo_link_list = photo_link_list[:len(photo_link_list)//2]
-# print(photo_link_list)
-print(photo_link_list)
+    page_url = "https://www.arabam.com" + car_page_links[list_item_num]
+    response = requests.get(page_url)
+    car_page_res = response.text
+
+    soup2 = BeautifulSoup(car_page_res, "html.parser")
+    photos = soup2.find_all("div", class_="swiper-slide")
+    values = soup2.find_all("div", class_="property-item")
+
+    for info in values:
+        info_key_list.append(info.find('div', class_="property-key").get_text(strip=True))
+        info_value_list.append(info.find('div', class_="property-value").get_text(strip=True))
+
+    for photo in photos:
+        photo_link_list.append(photo.find("img").get("data-src"))
+    photo_link_list = photo_link_list[:len(photo_link_list) // 2]
+
+    result = dict(zip(info_key_list, info_value_list))
+    print(page_url)
+    print(result)
+    print(photo_link_list)
+    redis_client.hset(f"info:{type}", mapping=result)
+
+# scrape_vehicle("otomobil")
