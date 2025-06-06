b6dcbd830543c4435133d284f3e8ee1912edc866
Ömer Faruk Yılmaz <omerfarukyy@hotmail.com>
2025-06-06 23:32:24 +0300

    scraping error 9 header change

--- backend/controllers/scraping.py
+++ backend/controllers/scraping.py
@@ -14,7 +14,9 @@ def scrape_vehicle():
                 # kiralik-araclar içeriği daha farklı
 
                 headers = {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+                    "Accept-Language": "tr-TR,tr;q=0.9,en;q=0.8",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
                 }
                 page_num = random.randint(1, 30)
                 minimal_page_num = random.randint(1, 5)
